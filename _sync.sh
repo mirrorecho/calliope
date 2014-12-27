9c39e66b2995735075fc80cee3420d4dbf480add
mirrorecho <info@randallwest.com>
2014-12-27 14:42:21 -0500

    adjustments to sync

--- _sync.sh
+++ _sync.sh
@@ -1,5 +1,8 @@
+# change directory to calliope directory (directory containint the sync script)
+cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 read -p "Enter commit message for CALLIOPE changes:" calliope_commit_msg
-git add .
+git add --all .
 git commit -m "$calliope_commit_msg"
 git pull --rebase
 git push
